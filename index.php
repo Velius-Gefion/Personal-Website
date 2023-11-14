9e39d11de34b92a35ab80de9aa730a618c51784e
Velius-Gefion <97680953+Velius-Gefion@users.noreply.github.com>
2023-11-14 18:19:05 +0800

    Temp Test

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title>Sei Illustrates</title>
+	<link href="bootstrap.min.css" rel="stylesheet">
+	<script src="jquery.min.js"></script>
+</head>
+<h1>Test Website</h1>
+</html>
+
+<style type="text/css">
+</script>
