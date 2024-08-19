698949d663ef8d60baba43477159adc0e8c13574
Velius-Gefion <97680953+Velius-Gefion@users.noreply.github.com>
2024-08-19 20:36:39 +0800

    ehhh

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename='/Personal-Website'>
     <App />
   </Router>
 );
